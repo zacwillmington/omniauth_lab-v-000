39bae8d2acebc6ad9c1821ca79d2134596435241
Zac Willmington <zac.willmington@gmail.com>
2018-08-01 02:28:09 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
 
     def create
         binding.pry
-        @user = User.find_or_create_by(:uid => auth['info']['uid'])
+        @user = User.find_or_create_by(:uid => auth['uid'])
+        session[:user_id] = @user.id
         binding.pry
     end
 
